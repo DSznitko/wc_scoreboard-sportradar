7d5350c1ead88b752373e6bc94efb5dc39610450
Daniel Sznitko <93942736+DSznitko@users.noreply.github.com>
2025-05-13 22:47:07 +0200

    RED: add start match failure test

---
+++ tests/Scoreboard.test.ts
@@ -0,0 +1,18 @@
+import { ScoreBoard } from "../src/ScoreBoard";
+
+describe("ScoreBoard", () => {
+  let scoreboard: ScoreBoard;
+
+  beforeEach(() => {
+    scoreboard = new ScoreBoard();
+  });
+
+  it("should start a new match with initial score 0-0", () => {
+    scoreboard.startMatch("Mexico", "Canada", 0, 0);
+
+    expect(scoreboard.matches[0].homeTeam).toBe("Mexico");
+    expect(scoreboard.matches[0].awayTeam).toBe("Canada");
+    expect(scoreboard.matches[0].homeScore).toBe(0);
+    expect(scoreboard.matches[0].awayScore).toBe(0);
+  });
+});
